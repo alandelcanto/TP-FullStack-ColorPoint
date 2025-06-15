dc4532f0c9d50bfc7184f0e6098e3eeb06715778
Nombre <adelcanto131@gmail.com>
2025-06-15 20:01:15 -0300

    funcionamiento index

---
+++ Frontend/js/index.js
@@ -0,0 +1,16 @@
+function init() {
+    sessionStorage.clear();
+
+    const nombre = document.querySelector("#nombreCliente");
+    const botonContinuar = document.querySelector(".boton-continuar");
+    botonContinuar.addEventListener("click", () => {
+        if (nombre.value != "") {
+            sessionStorage.setItem("nombreCliente", nombre.value);
+            console.log(nombre.value);
+            
+            window.location.href = "productos.html";
+        }
+    });
+}
+
+document.addEventListener("DOMContentLoaded", init);
